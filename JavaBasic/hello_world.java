93e749e3b09e6e6edc2d8928393dfb743ec8ec41
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-01 10:00:00 +0530

    added program hello_world.java

---
+++ JavaBasic/hello_world.java
@@ -0,0 +1,7 @@
+// 1	hello_world.java	Write a JavaScript program to print "Hello World" on the console.
+
+public class hello_world {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
