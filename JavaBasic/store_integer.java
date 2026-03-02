4293fb849de1de23dcb9450cfa4a1c9fac9a2e5b
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-02 10:00:00 +0530

    added program store_integer.java

---
+++ JavaBasic/store_integer.java
@@ -0,0 +1,8 @@
+// 5	store_integer.java	Store an integer value in a variable and print it.
+
+public class store_integer {
+    public static void main(String[] args) {
+        int num = 42;
+        System.out.println("Integer value: " + num);
+    }
+}
