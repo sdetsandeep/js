72dde7a88e40815b13454a8a5ba8ee70da17523d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-02 10:00:00 +0530

    added program store_string.java

---
+++ JavaBasic/store_string.java
@@ -0,0 +1,8 @@
+// 7	store_string.java	Store a string value in a variable and print it.
+
+public class store_string {
+    public static void main(String[] args) {
+        String message = "Hello, Java!";
+        System.out.println("String value: " + message);
+    }
+}
