bed98afb16ef5c80a40378b5674c701981ebf331
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-04 10:00:00 +0530

    added program multi_console_log.java

---
+++ JavaBasic/multi_console_log.java
@@ -0,0 +1,12 @@
+// 16	multi_console_log.java	Print multiple variables in a single console.log() statement.
+
+public class multi_console_log {
+    public static void main(String[] args) {
+        String name = "Raj";
+        int age = 25;
+        double marks = 88.5;
+        boolean passed = true;
+
+        System.out.println("Name: " + name + " | Age: " + age + " | Marks: " + marks + " | Passed: " + passed);
+    }
+}
