d5b9ecaba18853673d6fbd321aadce717db7524b
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-05 10:00:00 +0530

    added program 23_multiply_two.java

---
+++ JavaBasic/23_multiply_two.java
@@ -0,0 +1,10 @@
+// 23	23_multiply_two.java	Write a program to multiply two numbers.
+
+public class _23_multiply_two {
+    public static void main(String[] args) {
+        int a = 6, b = 7;
+        int product = a * b;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a * b = " + product);
+    }
+}
