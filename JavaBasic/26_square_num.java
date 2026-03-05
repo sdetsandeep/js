420ef04b7f0739675d0cf236f831d5ef712931de
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-05 10:00:00 +0530

    added program 26_square_num.java

---
+++ JavaBasic/26_square_num.java
@@ -0,0 +1,10 @@
+// 26	26_square_num.java	Calculate the square of a given number.
+
+public class _26_square_num {
+    public static void main(String[] args) {
+        int num = 9;
+        int square = num * num;
+        System.out.println("Number: " + num);
+        System.out.println("Square: " + square);
+    }
+}
