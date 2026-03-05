f7ddac59af12c9afa226b452aa53484b75864c1b
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-05 10:00:00 +0530

    added program 25_remainder.java

---
+++ JavaBasic/25_remainder.java
@@ -0,0 +1,10 @@
+// 25	25_remainder.java	Find the remainder when one number is divided by another using % operator.
+
+public class _25_remainder {
+    public static void main(String[] args) {
+        int a = 17, b = 5;
+        int remainder = a % b;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a % b = " + remainder);
+    }
+}
