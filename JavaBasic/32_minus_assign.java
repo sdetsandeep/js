c3e61f913cffe9019ad46a571d619249eed368e3
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-07 10:00:00 +0530

    added program 32_minus_assign.java

---
+++ JavaBasic/32_minus_assign.java
@@ -0,0 +1,9 @@
+// 32	32_minus_assign.java	Use -= operator to subtract a value from an existing variable.
+public class _32_minus_assign {
+    public static void main(String[] args) {
+        int num = 20;
+        System.out.println("Original: " + num);
+        num -= 8;
+        System.out.println("After num -= 8: " + num);
+    }
+}
