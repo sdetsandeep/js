39020e324df8468b750abef8a8e78bed1ad1edf5
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-07 10:00:00 +0530

    added program 33_mul_assign.java

---
+++ JavaBasic/33_mul_assign.java
@@ -0,0 +1,9 @@
+// 33	33_mul_assign.java	Use *= operator to multiply a variable's value.
+public class _33_mul_assign {
+    public static void main(String[] args) {
+        int num = 6;
+        System.out.println("Original: " + num);
+        num *= 4;
+        System.out.println("After num *= 4: " + num);
+    }
+}
