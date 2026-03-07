e9cdf61fab16453a71938002c7290440bdab3f36
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-07 10:00:00 +0530

    added program 35_average_three.java

---
+++ JavaBasic/35_average_three.java
@@ -0,0 +1,9 @@
+// 35	35_average_three.java	Calculate the average of three numbers.
+public class _35_average_three {
+    public static void main(String[] args) {
+        double a = 10, b = 20, c = 30;
+        double average = (a + b + c) / 3;
+        System.out.println("Numbers: " + a + ", " + b + ", " + c);
+        System.out.println("Average: " + average);
+    }
+}
