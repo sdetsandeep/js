78cd50eff43785027a5c6c46336e5ff5efbe1690
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-07 10:00:00 +0530

    added program 37_rect_perimeter.java

---
+++ JavaBasic/37_rect_perimeter.java
@@ -0,0 +1,9 @@
+// 37	37_rect_perimeter.java	Find the perimeter of a rectangle given its length and breadth.
+public class _37_rect_perimeter {
+    public static void main(String[] args) {
+        double length = 8, breadth = 5;
+        double perimeter = 2 * (length + breadth);
+        System.out.println("Length: " + length + " | Breadth: " + breadth);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
