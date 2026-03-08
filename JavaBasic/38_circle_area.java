7399f13322a2bb0a2c5484d28b0084b90cda1c83
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-08 10:00:00 +0530

    added program 38_circle_area.java

---
+++ JavaBasic/38_circle_area.java
@@ -0,0 +1,9 @@
+// 38	38_circle_area.java	Find the area of a circle given its radius using Math.PI.
+public class _38_circle_area {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = Math.PI * radius * radius;
+        System.out.println("Radius: " + radius);
+        System.out.printf("Area: %.2f%n", area);
+    }
+}
