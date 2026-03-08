af90cd10f35c3bec3094352c4d7e6caa3692c954
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-08 10:00:00 +0530

    added program 39_celsius_to_fahr.java

---
+++ JavaBasic/39_celsius_to_fahr.java
@@ -0,0 +1,9 @@
+// 39	39_celsius_to_fahr.java	Convert temperature from Celsius to Fahrenheit using the formula.
+public class _39_celsius_to_fahr {
+    public static void main(String[] args) {
+        double celsius = 37;
+        double fahrenheit = (celsius * 9.0 / 5) + 32;
+        System.out.println("Celsius: " + celsius);
+        System.out.println("Fahrenheit: " + fahrenheit);
+    }
+}
