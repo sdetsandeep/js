3808768f86ca9d58881c1c829f30f3bc43af015d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-09 10:00:00 +0530

    added program 48_less_equal.java

---
+++ JavaBasic/48_less_equal.java
@@ -0,0 +1,9 @@
+// 48	48_less_equal.java	Check whether a number is less than or equal to another using <=.
+public class _48_less_equal {
+    public static void main(String[] args) {
+        int a = 8, b = 10;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a <= b: " + (a <= b));
+        System.out.println("b <= a: " + (b <= a));
+    }
+}
