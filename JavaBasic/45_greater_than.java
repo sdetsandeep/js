b1efaedf2444617260bc9f6355426c1c8a9ea079
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-09 10:00:00 +0530

    added program 45_greater_than.java

---
+++ JavaBasic/45_greater_than.java
@@ -0,0 +1,9 @@
+// 45	45_greater_than.java	Check whether one number is greater than another using >.
+public class _45_greater_than {
+    public static void main(String[] args) {
+        int a = 15, b = 10;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a > b: " + (a > b));
+        System.out.println("b > a: " + (b > a));
+    }
+}
