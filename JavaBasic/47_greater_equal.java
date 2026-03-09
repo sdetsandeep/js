53cd3a9d53fa37f25fe4f74cf08ea0bd429fea5c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-09 10:00:00 +0530

    added program 47_greater_equal.java

---
+++ JavaBasic/47_greater_equal.java
@@ -0,0 +1,9 @@
+// 47	47_greater_equal.java	Check whether a number is greater than or equal to another using >=.
+public class _47_greater_equal {
+    public static void main(String[] args) {
+        int a = 10, b = 10;
+        System.out.println("a = " + a + ", b = " + b);
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a >= 5: " + (a >= 5));
+    }
+}
