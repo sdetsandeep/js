fdc98ade44b7ee7397b218c1cbf828b3aa632e27
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-10 10:00:00 +0530

    added program 51_logical_not.java

---
+++ JavaBasic/51_logical_not.java
@@ -0,0 +1,9 @@
+// 51	51_logical_not.java	Use logical NOT (!) to reverse the value of a boolean.
+public class _51_logical_not {
+    public static void main(String[] args) {
+        boolean isRaining = true;
+        boolean isSunny = false;
+        System.out.println("isRaining: " + isRaining + " | !isRaining: " + !isRaining);
+        System.out.println("isSunny: " + isSunny + " | !isSunny: " + !isSunny);
+    }
+}
