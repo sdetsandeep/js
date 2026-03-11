3b9274c55934e10be976f3c5bbb85227c0abc25f
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-11 10:00:00 +0530

    added program 55_vote_eligible.java

---
+++ JavaBasic/55_vote_eligible.java
@@ -0,0 +1,9 @@
+// 55	55_vote_eligible.java	Check if a person is eligible to vote based on age (>= 18).
+public class _55_vote_eligible {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("Age: " + age);
+        if (age >= 18) System.out.println("Eligible to vote.");
+        else System.out.println("Not eligible to vote.");
+    }
+}
