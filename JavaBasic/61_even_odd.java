660a59804ca7a908b0bd3b7b7b21297cf7d99f7e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-12 10:00:00 +0530

    added program 61_even_odd.java

---
+++ JavaBasic/61_even_odd.java
@@ -0,0 +1,9 @@
+// 61	61_even_odd.java	Check whether a given number is even or odd using if-else.
+public class _61_even_odd {
+    public static void main(String[] args) {
+        int num = 7;
+        System.out.println("Number: " + num);
+        if (num % 2 == 0) System.out.println(num + " is Even.");
+        else System.out.println(num + " is Odd.");
+    }
+}
