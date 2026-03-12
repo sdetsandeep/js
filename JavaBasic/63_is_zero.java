26135e3543ceb7b785c47d2dc236b013e85a563b
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-12 10:00:00 +0530

    added program 63_is_zero.java

---
+++ JavaBasic/63_is_zero.java
@@ -0,0 +1,9 @@
+// 63	63_is_zero.java	Check whether a given number is exactly zero.
+public class _63_is_zero {
+    public static void main(String[] args) {
+        int num = 0;
+        System.out.println("Number: " + num);
+        if (num == 0) System.out.println("The number is exactly zero.");
+        else System.out.println("The number is not zero.");
+    }
+}
