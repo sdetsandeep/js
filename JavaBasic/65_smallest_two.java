2e17f253e926429a7a73603bd69277e6fea7cb67
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-13 10:00:00 +0530

    added program 65_smallest_two.java

---
+++ JavaBasic/65_smallest_two.java
@@ -0,0 +1,9 @@
+// 65	65_smallest_two.java	Find and print the smallest of two numbers using if-else.
+public class _65_smallest_two {
+    public static void main(String[] args) {
+        int a = 34, b = 56;
+        System.out.println("a = " + a + ", b = " + b);
+        if (a < b) System.out.println("Smallest: " + a);
+        else System.out.println("Smallest: " + b);
+    }
+}
