de5b00b630b32f1f152ba98636f2e964bc98f88e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-14 10:00:00 +0530

    added program 74_pass_fail.java

---
+++ JavaBasic/74_pass_fail.java
@@ -0,0 +1,9 @@
+// 74	74_pass_fail.java	Check whether a student has passed or failed based on marks >= 40.
+public class _74_pass_fail {
+    public static void main(String[] args) {
+        int marks = 55;
+        System.out.println("Marks: " + marks);
+        if (marks >= 40) System.out.println("Result: PASS");
+        else System.out.println("Result: FAIL");
+    }
+}
