eb9a9ef498bd3d5653dca33cf052936a444a172c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-19 10:00:00 +0530

    added program 101_num_to_weekday.java

---
+++ JavaBasic/101_num_to_weekday.java
@@ -0,0 +1,16 @@
+// 101	101_num_to_weekday.java	Convert a number (1–7) to its corresponding weekday name.
+public class _101_num_to_weekday {
+    public static void main(String[] args) {
+        int num = 4;
+        switch (num) {
+            case 1: System.out.println("Monday"); break;
+            case 2: System.out.println("Tuesday"); break;
+            case 3: System.out.println("Wednesday"); break;
+            case 4: System.out.println("Thursday"); break;
+            case 5: System.out.println("Friday"); break;
+            case 6: System.out.println("Saturday"); break;
+            case 7: System.out.println("Sunday"); break;
+            default: System.out.println("Invalid. Enter 1-7.");
+        }
+    }
+}
