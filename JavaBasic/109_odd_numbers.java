aff28b9d37df3feb97c8a435f059689622d685b5
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-21 10:00:00 +0530

    added program 109_odd_numbers.java

---
+++ JavaBasic/109_odd_numbers.java
@@ -0,0 +1,8 @@
+// 109	109_odd_numbers.java	Print all odd numbers from 1 to 100 using a loop.
+public class _109_odd_numbers {
+    public static void main(String[] args) {
+        System.out.println("Odd numbers 1 to 100:");
+        for (int i = 1; i <= 100; i += 2) System.out.print(i + " ");
+        System.out.println();
+    }
+}
