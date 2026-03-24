a292aa9f37d685595241fa098968bf3dd77900eb
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-24 10:00:00 +0530

    added program 130_pyramid.java

---
+++ JavaBasic/130_pyramid.java
@@ -0,0 +1,11 @@
+// 130	130_pyramid.java	Print a centered pyramid star pattern using nested loops.
+public class _130_pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n-i; s++) System.out.print(" ");
+            for (int j = 1; j <= 2*i-1; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
