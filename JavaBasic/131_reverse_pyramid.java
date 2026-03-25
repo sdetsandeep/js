dd56c0052bef671cb3e068e9db8a7e59fd763d6e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-25 10:00:00 +0530

    added program 131_reverse_pyramid.java

---
+++ JavaBasic/131_reverse_pyramid.java
@@ -0,0 +1,11 @@
+// 131	131_reverse_pyramid.java	Print a reverse (inverted) centered pyramid pattern using loops.
+public class _131_reverse_pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = n; i >= 1; i--) {
+            for (int s = 1; s <= n-i; s++) System.out.print(" ");
+            for (int j = 1; j <= 2*i-1; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}
