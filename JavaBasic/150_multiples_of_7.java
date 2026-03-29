4447423e9eefb025ba673de26b7a4fa97f85dcf2
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-29 10:00:00 +0530

    added program 150_multiples_of_7.java

---
+++ JavaBasic/150_multiples_of_7.java
@@ -0,0 +1,8 @@
+// 150	150_multiples_of_7.java	Print all multiples of 7 from 1 to 100.
+public class _150_multiples_of_7 {
+    public static void main(String[] args) {
+        System.out.print("Multiples of 7: ");
+        for (int i = 7; i <= 100; i += 7) System.out.print(i + " ");
+        System.out.println();
+    }
+}
