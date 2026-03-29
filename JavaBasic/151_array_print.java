fa3dfb8a730e93c176efdb42c3ec0ec8b3882c72
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-03-29 10:00:00 +0530

    added program 151_array_print.java

---
+++ JavaBasic/151_array_print.java
@@ -0,0 +1,7 @@
+// 151	151_array_print.java	Create an array and print all its elements using a loop.
+public class _151_array_print {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        for (int i = 0; i < arr.length; i++) System.out.println("Index " + i + ": " + arr[i]);
+    }
+}
