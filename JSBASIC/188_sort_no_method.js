9a63bf4acccfc48d0c9632cdb8e6b959cc6a480d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-04-02 10:00:00 +0530

    Added 188_sort_no_method.js

---
+++ JSBASIC/188_sort_no_method.js
@@ -0,0 +1,12 @@
+// 188	188_sort_no_method.js	Sort an array without using the built-in sort() method (bubble sort).
+
+let arr = [64, 34, 25, 12, 22, 11, 90];
+let n = arr.length;
+for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+        if (arr[j] > arr[j + 1]) {
+            [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        }
+    }
+}
+console.log("Sorted (Bubble Sort):", arr);
