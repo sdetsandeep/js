6918b647c3229fe269a7e6f06e2b1c376183174b
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-01 13:00:00 +0530

    Added 31_plus_assign.js

---
+++ JSBASIC/31_plus_assign.js
@@ -0,0 +1,6 @@
+// 31	31_plus_assign.js	Use += operator to add a value to an existing variable.
+
+let num = 10;
+console.log("Original value:", num);
+num += 5;
+console.log("After num += 5:", num);
