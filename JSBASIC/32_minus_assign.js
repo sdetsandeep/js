f6a728258d5ddbf84619ed5260e1987b6f4970f2
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-01 14:00:00 +0530

    Added 32_minus_assign.js

---
+++ JSBASIC/32_minus_assign.js
@@ -0,0 +1,6 @@
+// 32	32_minus_assign.js	Use -= operator to subtract a value from an existing variable.
+
+let num = 20;
+console.log("Original value:", num);
+num -= 8;
+console.log("After num -= 8:", num);
