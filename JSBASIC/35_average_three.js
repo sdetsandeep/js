0c11b00c63dd33867618bf80e8199eaf2c2968da
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-02 12:00:00 +0530

    Added 35_average_three.js

---
+++ JSBASIC/35_average_three.js
@@ -0,0 +1,6 @@
+// 35	35_average_three.js	Calculate the average of three numbers.
+
+let a = 10, b = 20, c = 30;
+let average = (a + b + c) / 3;
+console.log("Numbers:", a, b, c);
+console.log("Average:", average);
