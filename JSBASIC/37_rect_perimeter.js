32b606d53c945dcd1b25e8562c61d7b6ec78a18f
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-02 14:00:00 +0530

    Added 37_rect_perimeter.js

---
+++ JSBASIC/37_rect_perimeter.js
@@ -0,0 +1,7 @@
+// 37	37_rect_perimeter.js	Find the perimeter of a rectangle given its length and breadth.
+
+let length = 8;
+let breadth = 5;
+let perimeter = 2 * (length + breadth);
+console.log("Length:", length, "| Breadth:", breadth);
+console.log("Perimeter of Rectangle:", perimeter);
