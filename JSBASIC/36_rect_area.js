3c232bc9f0193bb92b2d2cadc65d6a39d81a7059
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-02 13:00:00 +0530

    Added 36_rect_area.js

---
+++ JSBASIC/36_rect_area.js
@@ -0,0 +1,7 @@
+// 36	36_rect_area.js	Find the area of a rectangle given its length and breadth.
+
+let length = 8;
+let breadth = 5;
+let area = length * breadth;
+console.log("Length:", length, "| Breadth:", breadth);
+console.log("Area of Rectangle:", area);
