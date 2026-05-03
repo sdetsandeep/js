7711e49e5c0d3b7603944427cfc384682b3f3aec
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-03 14:00:00 +0530

    Added 42_compare_strict.js

---
+++ JSBASIC/42_compare_strict.js
@@ -0,0 +1,8 @@
+// 42	42_compare_strict.js	Compare two values using === (strict equality) and observe the difference.
+
+let a = 5;
+let b = "5";
+console.log("a =", a, "(number), b =", b, "(string)");
+console.log("a == b (loose):", a == b);
+console.log("a === b (strict):", a === b);
+console.log("Note: === checks both value and type");
