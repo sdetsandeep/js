77cc13db6a2cfff7c16d3866b022e5386c14f16f
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-03 10:00:00 +0530

    Added 38_circle_area.js

---
+++ JSBASIC/38_circle_area.js
@@ -0,0 +1,6 @@
+// 38	38_circle_area.js	Find the area of a circle given its radius using Math.PI.
+
+let radius = 7;
+let area = Math.PI * radius * radius;
+console.log("Radius:", radius);
+console.log("Area of Circle:", area.toFixed(2));
