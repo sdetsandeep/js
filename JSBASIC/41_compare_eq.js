f0056e2ec9cef6c7c4e163194f0381de805f705f
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-03 13:00:00 +0530

    Added 41_compare_eq.js

---
+++ JSBASIC/41_compare_eq.js
@@ -0,0 +1,7 @@
+// 41	41_compare_eq.js	Compare two numbers using == (loose equality) and print true/false.
+
+let a = 5;
+let b = "5";
+console.log("a =", a, "(number), b =", b, "(string)");
+console.log("a == b:", a == b);
+console.log("Note: == does type coercion");
