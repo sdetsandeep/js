c026ea23a2c7f297b6496883d631d1f37f0fb167
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-03 12:00:00 +0530

    Added 40_fahr_to_celsius.js

---
+++ JSBASIC/40_fahr_to_celsius.js
@@ -0,0 +1,6 @@
+// 40	40_fahr_to_celsius.js	Convert temperature from Fahrenheit to Celsius using the formula.
+
+let fahrenheit = 98.6;
+let celsius = (fahrenheit - 32) * 5/9;
+console.log("Fahrenheit:", fahrenheit);
+console.log("Celsius:", celsius.toFixed(2));
