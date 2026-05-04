98967c9ff4e00ee1567efd1aea23a439530ed304
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-04 10:00:00 +0530

    Added 43_compare_neq.js

---
+++ JSBASIC/43_compare_neq.js
@@ -0,0 +1,7 @@
+// 43	43_compare_neq.js	Compare two numbers using != operator and print the result.
+
+let a = 10;
+let b = 20;
+console.log("a =", a, "| b =", b);
+console.log("a != b:", a != b);
+console.log("a != 10:", a != 10);
