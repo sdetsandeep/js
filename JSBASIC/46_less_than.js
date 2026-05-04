0cd8315239fc5e8ca5ccc16733b919871dac4f2d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-04 13:00:00 +0530

    Added 46_less_than.js

---
+++ JSBASIC/46_less_than.js
@@ -0,0 +1,7 @@
+// 46	46_less_than.js	Check whether one number is less than another using <.
+
+let a = 7;
+let b = 12;
+console.log("a =", a, "| b =", b);
+console.log("a < b:", a < b);
+console.log("b < a:", b < a);
