37cd4bf959c4fa34df00c0599a9caa720abfa241
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-04 11:00:00 +0530

    Added 44_compare_strict_neq.js

---
+++ JSBASIC/44_compare_strict_neq.js
@@ -0,0 +1,7 @@
+// 44	44_compare_strict_neq.js	Compare two values using !== (strict not equal) operator.
+
+let a = 5;
+let b = "5";
+console.log("a =", a, "(number), b =", b, "(string)");
+console.log("a != b (loose):", a != b);
+console.log("a !== b (strict):", a !== b);
