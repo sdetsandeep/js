0971a62dd505cbd8fb676c091f67d57691b30eb1
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-04 12:00:00 +0530

    Added 45_greater_than.js

---
+++ JSBASIC/45_greater_than.js
@@ -0,0 +1,7 @@
+// 45	45_greater_than.js	Check whether one number is greater than another using >.
+
+let a = 15;
+let b = 10;
+console.log("a =", a, "| b =", b);
+console.log("a > b:", a > b);
+console.log("b > a:", b > a);
