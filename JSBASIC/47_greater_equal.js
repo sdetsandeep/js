30c722b282605f79e4512de6f86dfa890c2a86d3
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-04 14:00:00 +0530

    Added 47_greater_equal.js

---
+++ JSBASIC/47_greater_equal.js
@@ -0,0 +1,7 @@
+// 47	47_greater_equal.js	Check whether a number is greater than or equal to another using >=.
+
+let a = 10;
+let b = 10;
+console.log("a =", a, "| b =", b);
+console.log("a >= b:", a >= b);
+console.log("a >= 5:", a >= 5);
