92e0b1db80e6c52a1de3b5501274f679dcf66f66
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-05 12:00:00 +0530

    Added 50_logical_or.js

---
+++ JSBASIC/50_logical_or.js
@@ -0,0 +1,7 @@
+// 50	50_logical_or.js	Use logical OR (||) to check if at least one condition is true.
+
+let a = 5;
+let b = -3;
+console.log("a =", a, "| b =", b);
+console.log("a > 0 || b > 0:", a > 0 || b > 0);
+console.log("a < 0 || b < 0:", a < 0 || b < 0);
