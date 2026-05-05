6e381af42b462ba26a31a12cb8bb264566457d76
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-05 13:00:00 +0530

    Added 51_logical_not.js

---
+++ JSBASIC/51_logical_not.js
@@ -0,0 +1,6 @@
+// 51	51_logical_not.js	Use logical NOT (!) to reverse the value of a boolean.
+
+let isRaining = true;
+let isSunny = false;
+console.log("isRaining:", isRaining, "| !isRaining:", !isRaining);
+console.log("isSunny:", isSunny, "| !isSunny:", !isSunny);
