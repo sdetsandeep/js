c5ac7cccf3c2c2c5edf35f46578d6363f34b7bb5
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-05 14:00:00 +0530

    Added 52_between_10_100.js

---
+++ JSBASIC/52_between_10_100.js
@@ -0,0 +1,6 @@
+// 52	52_between_10_100.js	Check if a number lies between 10 and 100 (inclusive).
+
+let num = 55;
+let result = num >= 10 && num <= 100;
+console.log("Number:", num);
+console.log("Is between 10 and 100 (inclusive):", result);
