7687c4fff4ef7c5b138e43aad6c588d8e0cdc3c3
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-05 10:00:00 +0530

    Added 48_less_equal.js

---
+++ JSBASIC/48_less_equal.js
@@ -0,0 +1,7 @@
+// 48	48_less_equal.js	Check whether a number is less than or equal to another using <=.
+
+let a = 8;
+let b = 10;
+console.log("a =", a, "| b =", b);
+console.log("a <= b:", a <= b);
+console.log("b <= a:", b <= a);
