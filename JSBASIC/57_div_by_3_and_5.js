62f609e3e558263870f94ed631ea3993c96af4bd
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-06 14:00:00 +0530

    Added 57_div_by_3_and_5.js

---
+++ JSBASIC/57_div_by_3_and_5.js
@@ -0,0 +1,9 @@
+// 57	57_div_by_3_and_5.js	Check if a number is divisible by both 3 and 5.
+
+let num = 15;
+console.log("Number:", num);
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log(num, "is divisible by both 3 and 5.");
+} else {
+    console.log(num, "is NOT divisible by both 3 and 5.");
+}
