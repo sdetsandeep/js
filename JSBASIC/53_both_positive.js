7e6b2e1cf8b63de7302e145a035df95c907c0d07
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-06 10:00:00 +0530

    Added 53_both_positive.js

---
+++ JSBASIC/53_both_positive.js
@@ -0,0 +1,7 @@
+// 53	53_both_positive.js	Check whether both of two given numbers are positive.
+
+let a = 7;
+let b = 3;
+let bothPositive = a > 0 && b > 0;
+console.log("a =", a, "| b =", b);
+console.log("Both positive:", bothPositive);
