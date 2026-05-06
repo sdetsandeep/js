2152f780f3b066d9f0d039ed58bec2adabc7e788
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-06 12:00:00 +0530

    Added 55_vote_eligible.js

---
+++ JSBASIC/55_vote_eligible.js
@@ -0,0 +1,9 @@
+// 55	55_vote_eligible.js	Check if a person is eligible to vote based on age (>= 18).
+
+let age = 20;
+console.log("Age:", age);
+if (age >= 18) {
+    console.log("Eligible to vote.");
+} else {
+    console.log("Not eligible to vote.");
+}
