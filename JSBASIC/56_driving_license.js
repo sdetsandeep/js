70acb67bd762ab879f9becae794d1dda2676b943
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-06 13:00:00 +0530

    Added 56_driving_license.js

---
+++ JSBASIC/56_driving_license.js
@@ -0,0 +1,9 @@
+// 56	56_driving_license.js	Check if a person can apply for a driving license based on age.
+
+let age = 17;
+console.log("Age:", age);
+if (age >= 18) {
+    console.log("Can apply for a driving license.");
+} else {
+    console.log("Cannot apply. Minimum age is 18.");
+}
