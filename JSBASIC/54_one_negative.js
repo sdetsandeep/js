6eecf1b502f5ee7def1f6a35b3f59af76436d4d6
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-06 11:00:00 +0530

    Added 54_one_negative.js

---
+++ JSBASIC/54_one_negative.js
@@ -0,0 +1,7 @@
+// 54	54_one_negative.js	Check whether at least one of two numbers is negative.
+
+let a = 5;
+let b = -4;
+let oneNegative = a < 0 || b < 0;
+console.log("a =", a, "| b =", b);
+console.log("At least one is negative:", oneNegative);
