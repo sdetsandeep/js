b9a042d13f1dc5a9984d6eb2e295d5a0c8e5adc7
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-07 14:00:00 +0530

    Added 62_pos_neg.js

---
+++ JSBASIC/62_pos_neg.js
@@ -0,0 +1,11 @@
+// 62	62_pos_neg.js	Check whether a number is positive, negative, or zero.
+
+let num = -5;
+console.log("Number:", num);
+if (num > 0) {
+    console.log(num, "is Positive.");
+} else if (num < 0) {
+    console.log(num, "is Negative.");
+} else {
+    console.log("The number is Zero.");
+}
