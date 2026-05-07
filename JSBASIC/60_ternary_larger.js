ffe3358fb9c040e96fe3567cee493e88679d535c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-07 12:00:00 +0530

    Added 60_ternary_larger.js

---
+++ JSBASIC/60_ternary_larger.js
@@ -0,0 +1,7 @@
+// 60	60_ternary_larger.js	Use the ternary operator to find and print the larger of two numbers.
+
+let a = 18;
+let b = 25;
+let larger = a > b ? a : b;
+console.log("a =", a, "| b =", b);
+console.log("Larger number:", larger);
