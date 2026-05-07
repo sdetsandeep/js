1238ccc43ba2d0374048efc7745ceb385e4fb630
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-07 13:00:00 +0530

    Added 61_even_odd.js

---
+++ JSBASIC/61_even_odd.js
@@ -0,0 +1,9 @@
+// 61	61_even_odd.js	Check whether a given number is even or odd using if-else.
+
+let num = 7;
+console.log("Number:", num);
+if (num % 2 === 0) {
+    console.log(num, "is Even.");
+} else {
+    console.log(num, "is Odd.");
+}
