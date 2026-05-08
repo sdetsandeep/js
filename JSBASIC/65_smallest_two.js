f4f06f86ab98a5c93bdc4d6b74353321e361bcf2
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-08 12:00:00 +0530

    Added 65_smallest_two.js

---
+++ JSBASIC/65_smallest_two.js
@@ -0,0 +1,10 @@
+// 65	65_smallest_two.js	Find and print the smallest of two numbers using if-else.
+
+let a = 34;
+let b = 56;
+console.log("a =", a, "| b =", b);
+if (a < b) {
+    console.log("Smallest:", a);
+} else {
+    console.log("Smallest:", b);
+}
