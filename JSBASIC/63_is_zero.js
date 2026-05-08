8607d806f6ffcc2348d5fdd3f253e96925a1fa2d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-08 10:00:00 +0530

    Added 63_is_zero.js

---
+++ JSBASIC/63_is_zero.js
@@ -0,0 +1,9 @@
+// 63	63_is_zero.js	Check whether a given number is exactly zero.
+
+let num = 0;
+console.log("Number:", num);
+if (num === 0) {
+    console.log("The number is exactly zero.");
+} else {
+    console.log("The number is not zero.");
+}
