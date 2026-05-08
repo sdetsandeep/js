ac7354e20b914c42ffd987c85c27f1d8e0479ac8
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-08 11:00:00 +0530

    Added 64_largest_two.js

---
+++ JSBASIC/64_largest_two.js
@@ -0,0 +1,10 @@
+// 64	64_largest_two.js	Find and print the largest of two numbers using if-else.
+
+let a = 34;
+let b = 56;
+console.log("a =", a, "| b =", b);
+if (a > b) {
+    console.log("Largest:", a);
+} else {
+    console.log("Largest:", b);
+}
