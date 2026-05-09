566b876572cdf0b19e90952b665fe917ee651c4d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-09 10:00:00 +0530

    Added 73_is_digit.js

---
+++ JSBASIC/73_is_digit.js
@@ -0,0 +1,9 @@
+// 73	73_is_digit.js	Check whether a given character is a digit (0–9).
+
+let ch = '7';
+console.log("Character:", ch);
+if (ch >= '0' && ch <= '9') {
+    console.log(ch, "is a Digit.");
+} else {
+    console.log(ch, "is NOT a Digit.");
+}
