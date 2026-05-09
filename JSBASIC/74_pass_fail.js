dc7d3021a6cb4d67a78e73448bb12c123a4feb9e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-09 11:00:00 +0530

    Added 74_pass_fail.jscls

---
+++ JSBASIC/74_pass_fail.js
@@ -0,0 +1,9 @@
+// 74	74_pass_fail.js	Check whether a student has passed or failed based on marks >= 40.
+
+let marks = 55;
+console.log("Marks:", marks);
+if (marks >= 40) {
+    console.log("Result: PASS");
+} else {
+    console.log("Result: FAIL");
+}
