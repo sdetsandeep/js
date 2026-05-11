78ca309b3544eb237a37f81e6f51abd721d29bae
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-11 14:00:00 +0530

    Added 82_minor_adult.js

---
+++ JSBASIC/82_minor_adult.js
@@ -0,0 +1,9 @@
+// 82	82_minor_adult.js	Check if a person is a minor (<18) or an adult (>=18).
+
+let age = 15;
+console.log("Age:", age);
+if (age >= 18) {
+    console.log("Adult.");
+} else {
+    console.log("Minor.");
+}
