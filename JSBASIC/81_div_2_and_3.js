88a153f775dbdd7d0319e5affefe5b065d14919c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-11 13:00:00 +0530

    Added 81_div_2_and_3.js

---
+++ JSBASIC/81_div_2_and_3.js
@@ -0,0 +1,9 @@
+// 81	81_div_2_and_3.js	Find whether a number is divisible by both 2 and 3.
+
+let num = 12;
+console.log("Number:", num);
+if (num % 2 === 0 && num % 3 === 0) {
+    console.log(num, "is divisible by both 2 and 3.");
+} else {
+    console.log(num, "is NOT divisible by both 2 and 3.");
+}
