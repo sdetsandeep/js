eef96d4e4a010cc1b77ed541d75bc8d0cab3fb3d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-12 10:00:00 +0530

    Added 83_multiple_of_10.js

---
+++ JSBASIC/83_multiple_of_10.js
@@ -0,0 +1,9 @@
+// 83	83_multiple_of_10.js	Find whether a given number is a multiple of 10.
+
+let num = 40;
+console.log("Number:", num);
+if (num % 10 === 0) {
+    console.log(num, "is a multiple of 10.");
+} else {
+    console.log(num, "is NOT a multiple of 10.");
+}
