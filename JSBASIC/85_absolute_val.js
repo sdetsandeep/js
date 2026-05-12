c6dbedac217a143be371d2fa67547f6e3c220159
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-12 12:00:00 +0530

    Added 85_absolute_val.js

---
+++ JSBASIC/85_absolute_val.js
@@ -0,0 +1,6 @@
+// 85	85_absolute_val.js	Find the absolute value of a number without using Math.abs().
+
+let num = -42;
+console.log("Number:", num);
+let absolute = num < 0 ? -num : num;
+console.log("Absolute Value:", absolute);
