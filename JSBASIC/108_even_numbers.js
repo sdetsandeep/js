8e741bfa699d6dcdd04fe00e6bde91304ee32f91
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-16 10:00:00 +0530

    Added 108_even_numbers.js

---
+++ JSBASIC/108_even_numbers.js
@@ -0,0 +1,7 @@
+// 108	108_even_numbers.js	Print all even numbers from 1 to 100 using a loop.
+
+console.log("Even numbers from 1 to 100:");
+for (let i = 2; i <= 100; i += 2) {
+    process.stdout.write(i + " ");
+}
+console.log();
