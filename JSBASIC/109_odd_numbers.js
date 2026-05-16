8c00633c6473044099df685313ae81e33383d7c3
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-16 11:00:00 +0530

    Added 109_odd_numbers.js

---
+++ JSBASIC/109_odd_numbers.js
@@ -0,0 +1,7 @@
+// 109	109_odd_numbers.js	Print all odd numbers from 1 to 100 using a loop.
+
+console.log("Odd numbers from 1 to 100:");
+for (let i = 1; i <= 100; i += 2) {
+    process.stdout.write(i + " ");
+}
+console.log();
