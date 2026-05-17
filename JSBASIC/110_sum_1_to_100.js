a55f0a15acfbf378f356db167b6f0962eeaa2290
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-17 12:00:00 +0530

    Added 110_sum_1_to_100.js

---
+++ JSBASIC/110_sum_1_to_100.js
@@ -0,0 +1,7 @@
+// 110	110_sum_1_to_100.js	Find the sum of all numbers from 1 to 100 using a loop.
+
+let sum = 0;
+for (let i = 1; i <= 100; i++) {
+    sum += i;
+}
+console.log("Sum of 1 to 100:", sum);
