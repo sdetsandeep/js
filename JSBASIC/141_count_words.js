82bad2a3243dbbd4151c2dbabc47e1c39d6a907d
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-23 13:00:00 +0530

    Added 141_count_words.js

---
+++ JSBASIC/141_count_words.js
@@ -0,0 +1,6 @@
+// 141	141_count_words.js	Count the number of words in a given sentence.
+
+let str = "JavaScript is fun to learn";
+let words = str.trim().split(/\s+/);
+console.log("String:", str);
+console.log("Number of words:", words.length);
