8e7f30496d4b57d34b43cc688ba853abd343d996
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-24 10:00:00 +0530

    Added 143_print_chars.js

---
+++ JSBASIC/143_print_chars.js
@@ -0,0 +1,8 @@
+// 143	143_print_chars.js	Print each character of a string one by one using a loop.
+
+let str = "JavaScript";
+console.log("String:", str);
+console.log("Characters:");
+for (let i = 0; i < str.length; i++) {
+    console.log("Index " + i + ": " + str[i]);
+}
