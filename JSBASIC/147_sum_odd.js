4edd630a511deaf5fcf9f80afc0f5b47b0565d92
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-25 14:00:00 +0530

    Added 147_sum_odd.js

---
+++ JSBASIC/147_sum_odd.js
@@ -0,0 +1,7 @@
+// 147	147_sum_odd.js	Find the sum of all odd numbers from 1 to 100.
+
+let sum = 0;
+for (let i = 1; i <= 100; i += 2) {
+    sum += i;
+}
+console.log("Sum of all odd numbers from 1 to 100:", sum);
