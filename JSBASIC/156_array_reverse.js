e63e330a269c126b83ffde9c682ba9ad6c4758b5
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-26 13:00:00 +0530

    Added 156_array_reverse.js

---
+++ JSBASIC/156_array_reverse.js
@@ -0,0 +1,6 @@
+// 156	156_array_reverse.js	Reverse an array using the built-in reverse() method.
+
+let arr = [1, 2, 3, 4, 5];
+console.log("Original:", arr);
+arr.reverse();
+console.log("Reversed:", arr);
