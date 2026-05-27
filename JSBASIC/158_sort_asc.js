e695a57e30cab5f808441a0e7468ea7208c6b5f7
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-27 10:00:00 +0530

    Added 158_sort_asc.js

---
+++ JSBASIC/158_sort_asc.js
@@ -0,0 +1,6 @@
+// 158	158_sort_asc.js	Sort an array of numbers in ascending order.
+
+let arr = [64, 25, 12, 22, 11];
+console.log("Original:", arr);
+arr.sort((a, b) => a - b);
+console.log("Ascending order:", arr);
