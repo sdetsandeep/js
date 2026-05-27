132e9389c7de92fcd0a67129f658c3d398055e38
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-27 11:00:00 +0530

    Added 159_sort_desc.js

---
+++ JSBASIC/159_sort_desc.js
@@ -0,0 +1,6 @@
+// 159	159_sort_desc.js	Sort an array of numbers in descending order.
+
+let arr = [64, 25, 12, 22, 11];
+console.log("Original:", arr);
+arr.sort((a, b) => b - a);
+console.log("Descending order:", arr);
