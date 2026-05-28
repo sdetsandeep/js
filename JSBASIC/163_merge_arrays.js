b5e01aed50e6d9225fe640142aed41d32829b5e3
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-28 10:00:00 +0530

    Added 163_merge_arrays.js

---
+++ JSBASIC/163_merge_arrays.js
@@ -0,0 +1,8 @@
+// 163	163_merge_arrays.js	Merge two arrays into one using concat() or spread operator.
+
+let arr1 = [1, 2, 3];
+let arr2 = [4, 5, 6];
+let merged = [...arr1, ...arr2];
+console.log("Array 1:", arr1);
+console.log("Array 2:", arr2);
+console.log("Merged:", merged);
