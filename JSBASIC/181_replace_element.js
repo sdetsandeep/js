e3af5c62f2aca0da5b50248d9a1cab51f6ccdd8a
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-05-31 13:00:00 +0530

    Added 181_replace_element.js

---
+++ JSBASIC/181_replace_element.js
@@ -0,0 +1,8 @@
+// 181	181_replace_element.js	Replace an element at a specific position in an array.
+
+let arr = [10, 20, 30, 40, 50];
+let pos = 2;
+let newVal = 99;
+console.log("Original:", arr);
+arr[pos] = newVal;
+console.log("After replacing index", pos, "with", newVal + ":", arr);
