32823176175e40207295c4324028db61fbe9df42
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-01 10:00:00 +0530

    Added 23_multiply_two.js

---
+++ JSBASIC/23_multiply_two.js
@@ -0,0 +1,8 @@
+//	23_multiply_two.js	Write a program to multiply two numbers.
+//21_add_two.js   Add two number 
+ let num1 = 10;
+let num2 = 20;
+
+let mul = num1 * num2;
+
+console.log("Mul =", mul);
