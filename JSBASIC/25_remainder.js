5ea3a2f3a0ed10e3dd64e30926ba3f17ab153d9e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-02 10:00:00 +0530

    Added 25_remainder.js

---
+++ JSBASIC/25_remainder.js
@@ -0,0 +1,8 @@
+// 25 Find the remainder when one number is divided by another using % operator.
+
+let num1 = 17;
+let num2 = 5;
+
+let remainder = num1 % num2;
+
+console.log("Remainder =", remainder);
