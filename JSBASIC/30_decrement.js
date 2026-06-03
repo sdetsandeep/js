4b3cf2de0f1c630a1f29681adf7d8075b4bde7ec
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-03 10:00:00 +0530

    updated 30_decrement.js file

---
+++ JSBASIC/30_decrement.js
@@ -0,0 +1,8 @@
+// 30	30_decrement.js	Decrement a variable using the -- operator (both pre and post).
+
+let a = 10;
+console.log("Original value:", a);
+console.log("Post-decrement (a--):", a--);
+console.log("After post-decrement:", a);
+console.log("Pre-decrement (--a):", --a);
+console.log("After pre-decrement:", a);
