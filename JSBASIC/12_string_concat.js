d503533778d4269f33c0ddf0d927b2ee1b096b3e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 03:45:41 +0530

    Concatenate two strings s1 and s2
    Concatenate two strings s1 and s2 and file name rename of some program in list

---
+++ JSBASIC/12_string_concat.js
@@ -0,0 +1,7 @@
+//12	string_concat.js	Concatenate two strings using + operator and print the result.
+
+let s1= "Sandeep";
+let s2= " Kumar";
+let s3;
+s3= s1+s2;
+ console.log("The Concatenate two strings s1 and s2 using + operator is : "+s3);
