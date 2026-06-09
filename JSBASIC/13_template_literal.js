1f9855fb76235d87f5509dc54349eae746eca235
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 03:51:09 +0530

    template_literal.js	Use template literals (backticks) to print a formatted sentence with variables.
    template_literal.js	Use template literals (backticks) to print a formatted sentence with variables.

---
+++ JSBASIC/13_template_literal.js
@@ -0,0 +1,8 @@
+//13	template_literal.js	Use template literals (backticks) to print a formatted sentence with variables.
+
+let name = "Sandeep";
+let age = 25;
+let city = "Rishikesh";
+
+// Using template literals (backticks)
+console.log(`My name is ${name}, I am ${age} years old, and I live in ${city}.`);
