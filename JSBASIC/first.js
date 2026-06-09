9f136d8382d431da591480170c6416e192e1011e
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-09 19:55:27 +0530

    Create first.js

---
+++ JSBASIC/first.js
@@ -0,0 +1,20 @@
+console.log("Hello Sandeep KUmar Sharma");
+let char='a';
+let name="sandeep";
+let a=10;
+let b=20;
+let str="Sandeep kumar sh";
+let bool= true;
+let fbool= false;
+let dbl=30.555547;
+
+
+console.log ("The value of char = "+char);
+console.log ("The value of name = "+name);
+console.log ("The value of a = "+a);
+console.log ("The value of b = "+b);
+//console.log ("The value of c = "+c);
+console.log ("The value of st = "+str);
+console.log ("The value of bool = "+bool);
+console.log ("The value of fbool = "+fbool);
+console.log ("The value of dbl = "+dbl);
