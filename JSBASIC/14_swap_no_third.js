02068f6fe0509f2abdca1ec357c55d37dcb4e15c
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 03:54:37 +0530

    //14	swap_no_third.js
    //14	swap_no_third.js

---
+++ JSBASIC/14_swap_no_third.js
@@ -0,0 +1,15 @@
+
+//14	swap_no_third.js	Swap the values of two variables without using a third variable.
+
+let a=10;
+let b=20 
+let c;
+console.log("Before swapping  the values  are ... ");
+console.log("Before swapping  the value of a is  ... "+a + "   and the value of b ="+b);
+c=a;
+a=b;
+b=c;
+
+console.log("After swapping  the values  are ... ");
+
+console.log("Before swapping  the value of a is  ... "+a + "   and the value of b ="+b);
