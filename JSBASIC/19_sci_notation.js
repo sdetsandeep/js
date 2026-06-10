33ead6093ab7e6c7a739de9a58cd96952e5f5cc6
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 18:41:38 +0530

    Create 19_sci_notation.js

---
+++ JSBASIC/19_sci_notation.js
@@ -0,0 +1,8 @@
+//19	19_sci_notation.js	Store a number in scientific notation (e.g. 5e6) and print it.
+
+
+// Store a number in scientific notation
+let number = 5e6;
+
+// Print the value
+console.log("Scientific Notation Value:", number);
