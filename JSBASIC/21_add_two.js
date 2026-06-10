992569c4a55bd9db3cb24fa7282391632dfc77a6
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 18:46:24 +0530

    program 20,21, 22

---
+++ JSBASIC/21_add_two.js
@@ -0,0 +1,7 @@
+//21_add_two.js   Add two number 
+ let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+
+console.log("Sum =", sum);
---
+++ JSBASIC/22_Subctract_two.js
@@ -0,0 +1,7 @@
+//22_subtract_two.js – Subtract Two Numbers
+let num1 = 20;
+let num2 = 10;
+
+let difference = num1 - num2;
+
+console.log("Difference =", difference);
