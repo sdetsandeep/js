47fc4edb05c8e9a7eb53669823029e424d159e26
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-10 18:43:18 +0530

    Create 20_multiline_string.js

---
+++ JSBASIC/20_multiline_string.js
@@ -0,0 +1,6 @@
+//20	//20_multiline_string.js	Print a multiline string using template literals or \n escape character.
+// multiline_newline.js
+
+let message = "Welcome to JavaScript\nThis is the second line.\nThis is the third line.";
+
+console.log(message);
