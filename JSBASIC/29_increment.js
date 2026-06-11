b0cec824e83daabe293e5a8dd5771d9de0c8b811
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-11 17:00:06 +0530

    Create 29_increment.js

---
+++ JSBASIC/29_increment.js
@@ -0,0 +1,8 @@
+// 29	29_increment.js	Increment a variable using the ++ operator (both pre and post).
+
+let a = 5;
+console.log("Original value:", a);
+console.log("Post-increment (a++):", a++);
+console.log("After post-increment:", a);
+console.log("Pre-increment (++a):", ++a);
+console.log("After pre-increment:", a);
