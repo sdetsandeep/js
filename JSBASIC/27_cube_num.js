8ceeb67a5bbbb39bfe6d55b218ee8607b6244abf
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-11 16:58:39 +0530

    program 27 and  28 added

---
+++ JSBASIC/27_cube_num.js
@@ -0,0 +1,6 @@
+// 27	27_cube_num.js	Calculate the cube of a given number.
+
+let num = 3;
+let cube = num * num * num;
+console.log("Number:", num);
+console.log("Cube:", cube);
---
+++ JSBASIC/28_power_operator.js
@@ -0,0 +1,6 @@
+// 28	28_power_operator.js	Calculate the power of a number using the ** exponentiation operator.
+
+let base = 2;
+let exponent = 5;
+let result = base ** exponent;
+console.log(`${base} ** ${exponent} =`, result);
