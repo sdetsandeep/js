744af5365ae6124e9d3ba5dcbedd3bc71e0b402f
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-15 17:33:33 +0530

    Update 201_type_conversion.js

--- JSBASIC/201_type_conversion.js
+++ JSBASIC/201_type_conversion.js
@@ -3,3 +3,16 @@ console.log(typeof value); // boolean
 
 value = String(value); // now value is a string "true"
 console.log(typeof value); // string
+
+let str = "123";
+console.log("str =", str);
+console.log("Type of str =", typeof str);
+
+let num = Number(str);
+
+console.log("num =", num);
+console.log("Type of num =", typeof num);
+
+let age = Number("an arbitrary string instead of a number");
+
+alert(age); // NaN, conversion failed
