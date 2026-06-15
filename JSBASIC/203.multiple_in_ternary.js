4c67768ee1c88e83b2bd4895863ddba0fd64f762
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-16 03:21:48 +0530

    multiple in ternary operator

--- JSBASIC/203.multiple_in_ternary.js
+++ JSBASIC/203.multiple_in_ternary.js
@@ -1,6 +1,7 @@
-let age = 25;
+let age = "AaB";
 
 let message =
+    isNaN(age) ? "Please enter a valid number" :
     age < 3 ? "Hi Baby" :
     age < 18 ? "Abhi aap 18 se kam ho" :
     age == 18 ? "Aap exactly 18 ke ho" :
