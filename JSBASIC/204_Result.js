7b9e9ffc1ceb9042c349f156e17ae978f5badfa2
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-16 10:57:10 +0530

    Create 204_Result.js

---
+++ JSBASIC/204_Result.js
@@ -0,0 +1,14 @@
+let hindi,english,math;
+
+const prompt = require("prompt-sync")();
+
+ hindi = prompt("Enter Marks in Hindi :... ");
+
+ english = prompt("Enter Marks in english :... ");
+
+ math = prompt("Enter Marks of math :.. ");
+
+
+console.log("Marks in Hindi  =", Hindi);
+console.log("Marks in English  =", Engllish);
+console.log("Marks in Math  =", Math);
