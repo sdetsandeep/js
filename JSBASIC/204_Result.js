f1f32f0f3d5b9ca39b98488e727719685b70fe30
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-16 11:22:43 +0530

    result.js file
    Student result project

--- JSBASIC/204_Result.js
+++ JSBASIC/204_Result.js
@@ -1,14 +1,55 @@
-let hindi,english,math;
+let hindi, english, math;
+
+console.log("HI Users... Please proceed to complete operation...");
 
 const prompt = require("prompt-sync")();
 
- hindi = prompt("Enter Marks in Hindi :... ");
+hindi = Number(prompt("Enter Marks in Hindi: "));
+english = Number(prompt("Enter Marks in English: "));
+math = Number(prompt("Enter Marks in Math: "));
 
- english = prompt("Enter Marks in english :... ");
+console.log("Marks in Hindi =", hindi);
+console.log("Marks in English =", english);
+console.log("Marks in Math =", math);
 
- math = prompt("Enter Marks of math :.. ");
+
+let per= (hindi+english+math)/3;
+
+console.log (" ============================================================ ");
+ 
+console.log (" Your Percentage is  ..... "+ per);
 
 
-console.log("Marks in Hindi  =", Hindi);
-console.log("Marks in English  =", Engllish);
-console.log("Marks in Math  =", Math);
+if(per<35)
+{
+    console.log(" You Are Failed........ Please try again....");
+
+    console.log (" ============================================================ ");
+}
+else if (per>=35 && per<45)
+{
+     console.log(" You 3rd Division ... .");
+     console.log (" ============================================================ ");
+}
+else if (per>=45 && per<60)
+{
+     console.log(" You got  2nd Division......");
+     console.log (" ============================================================ ");
+}
+
+else if (per>=60 )
+{
+     if( per>=75)
+     {
+        console.log(" You got Ist Division with Distinctions.. Honor ..");
+        console.log (" ============================================================ ");
+     }
+
+     else 
+     {
+        console.log(" You got  Ist  Division......");
+        console.log (" ============================================================ ");
+     }
+    
+    
+}
