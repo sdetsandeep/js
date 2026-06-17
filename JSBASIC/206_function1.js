f7672164e0bba71cee96b89b7ca4336fe9dedd75
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-17 18:38:07 +0530

    calculator functions
    calculator functions

---
+++ JSBASIC/206_function1.js
@@ -0,0 +1,7 @@
+
+function showMessge()
+{
+console.log("Hello sandeep Kumar sharma this is the function to show only message ....");
+}
+
+showMessge();
---
+++ JSBASIC/207_function_calculator.js
@@ -0,0 +1,37 @@
+
+function sum ( x, y)
+{
+    let sm= x+y;
+    console.log("Sum of given two number is = "+sm);
+}
+
+function sub ( x,  y)
+{
+    let sb= x-y;
+    console.log("Subtraction of given two number is = "+sb);
+}
+
+function mul ( x,  y)
+{
+    let ml= x*y;
+    console.log("Multiplication of given two number is = "+ml);
+}
+function div ( x,  y)
+{
+    let dv= x/y;
+    console.log("Division of given two number is = "+dv);
+}
+
+
+function calci()
+{
+    let x=20;
+    let y=5;
+    sum(x,y);
+    sub(x,y);
+    mul(x,y);
+    div(x,y);
+
+}
+
+calci();
