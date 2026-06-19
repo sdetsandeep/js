112e20668267969766e2e28833d6bc8652603172
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-19 14:04:45 +0530

    Arrray exercise
    Arrray exercise

---
+++ JSBASIC/209_Array1.js
@@ -0,0 +1,25 @@
+// Creating an Empty Array
+let a = [];
+console.log("Array a values are ... "+a);
+
+// Creating an Array and Initializing with Values
+let b = [10, 20, 30];
+console.log("Array b values are ... "+b);
+
+// Creating and Initializing an array with values
+let c = new Array(10, 20, 30);
+
+console.log(c);
+
+//1. Accessing Elements of an Array
+//Any element in the array can be accessed using the index number. The index in the arrays starts with 0.
+
+
+
+
+// Creating an Array and Initializing with Values
+let d = ["HTML", "CSS", "JS"];
+​
+// Accessing Array Elements
+console.log(d[0]);
+console.log(d[1]);
---
+++ JSBASIC/210_Array_push_pop.js
@@ -0,0 +1,19 @@
+/* 5. Adding Elements to the Array
+Elements can be added to the array using methods like push() and unshift().
+
+The push() method add the element to the end of the array.
+The unshift() method add the element to the starting of the array. */
+
+
+
+
+// Creating an Array and Initializing with Values
+let a = ["HTML", "CSS", "JS"];
+
+// Add Element to the end of Array
+a.push("Node.js");
+
+// Add Element to the beginning
+a.unshift("Web Development");
+
+console.log(a);
