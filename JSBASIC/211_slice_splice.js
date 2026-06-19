a0c4e44060cc3443fe17e342f0d0527510143f83
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-19 14:33:39 +0530

    Update 211_slice_splice.js

--- JSBASIC/211_slice_splice.js
+++ JSBASIC/211_slice_splice.js
@@ -23,5 +23,25 @@ console.log(result);
 console.log("\nOriginal Array After slice():");
 console.log(fruits);
 
-// Splice() Method 
+// Splice() Method          .. Remove Element
 console.log ("====================Splice () Method =====================================");
+
+let arr = [10, 20, 30, 40, 50];
+
+console.log("Before slice:", arr);
+
+let slicedArr = arr.slice(1, 4);
+
+console.log("Slice Result:", slicedArr);
+
+console.log("After slice:", arr);
+
+console.log("\n====================\n");
+
+console.log("Before splice:", arr);
+
+let deletedArr = arr.splice(1, 2);
+
+console.log("Deleted Elements:", deletedArr);
+
+console.log("After splice:", arr);
