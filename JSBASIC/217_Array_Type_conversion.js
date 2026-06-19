504c46a6ce524867f5244ab1d9fdf21511487995
Sandeep Kumar Sharma <sdet.monster.sandeep@gmail.com>
2026-06-19 17:20:30 +0530

    Update 217_Array_Type_conversion.js

--- JSBASIC/217_Array_Type_conversion.js
+++ JSBASIC/217_Array_Type_conversion.js
@@ -1,5 +1,15 @@
 // Creating an Array and Initializing with Values
 let a = ["HTML", "CSS", "JS"];
+ 
+console.log("Before converting into ")
+
+console.log(a);
 
 // Convert array ot String
+console.log("After  converting into ")
 console.log(a.toString());
+
+//Check the type of an array .. 
+
+console.log ( typeof(a));
+
